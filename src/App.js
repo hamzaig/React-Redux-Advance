5586240851cf580552795167ee658a2b4b587310
hamzaig <hamzaig@yahoo.com>
2021-11-10 02:01:10 +0500

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/uiRedux';
 
-
+let isInitail = true;
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch("https://react-http-c28cc-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
-  }, [cart])
+    const sendCartData = async () => {
+
+      dispatch(uiActions.showNotification({
+        status: "Pending",
+        title: "Sending ...",
+        message: "Sending Cart Data ...",
+      }));
+
+      const response = await fetch("https://react-http-c28cc-default-rtdb.firebaseio.com/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        dispatch(uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending Cart Data Failed",
+        }));
+        return;
+      }
+
+      dispatch(uiActions.showNotification({
+        status: "success",
+        title: "Success!",
+        message: "Sent Cart Data Successfully",
+      }));
+    }
+
+    if (isInitail) {
+      isInitail = false;
+      return;
+    }
+
+    sendCartData();
+
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification {...notification} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
